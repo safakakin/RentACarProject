6da95f8cecda5b2b8f3ecf7a6bf029d9f2681ce4
mariami <safakakin@hotmail.com.tr>
2022-10-10 14:31:41 +0300

    New entities has been added.

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+	public class User:IEntity
+	{
+		public int userId { get; set; }
+		public string userFirstName { get; set; }
+		public string userLastName { get; set; }
+		public string userEmail { get; set; }
+		public string userPassword { get; set; }
+	}
+}
+
